a5b508e5eab12bfd23f879175898e9d96f1a52be
Thomas <O581051@NAEAST.AD.JPMORGANCHASE.com>
2017-06-26 23:52:29 -0400

    Completed TC-Microlabs-loops

--- src/main/java/io/zipcode/microlabs/loops/Loops.java
+++ src/main/java/io/zipcode/microlabs/loops/Loops.java
@@ -1,5 +1,6 @@
 package io.zipcode.microlabs.loops;
 
+//import java.util.Collections; //this is not used, wanted to use for problem with reverse loop TBC
 
 public class Loops {
 
@@ -9,6 +10,93 @@ public class Loops {
             response +="\n" + input[x];
         }
         return response;
+        
+    }
+    
+    public static String printLastElement(String[] input){
+        String response = "*** Output ***";
+        int x = (input.length - 1);
+            response +="\n" + input[x];
+            return response;
+            
+        //both methods work
+        //for(int x=0; x< input.length; x = input.length - 1){
+           // response +="\n" + input[x];
+       // }
+        
     }
 
+    public static String printLastButOne(String[] input){
+        String response = "*** Output ***";
+        int x = (input.length - 2);
+            response +="\n" + input[x];
+        
+            return response;
+    }
+
+    public static String printReverse(String[] input){
+        String response = "*** Output ***";
+        for(int x= input.length - 1; x>=0; x--) {
+            response +="\n" + input[x];
+        }
+        return response;
+        
+        /* Would like to learn to use ArrayList
+        Collections.reverse(input).asList;
+        */
+        
+        
+    }
+
+    public static Boolean printPalindrome(String[] input){ 	
+        // Cool efficient solution I found digging online :)
+        int i1 = 0;
+        int i2 = input.length -1;
+        while (i2 > i1) {
+        	if (input[i1] != input[i2]) {
+        		return false;
+        	}
+        	++i1;
+        	--i2;
+        }
+		return true;
+		
+    	/* this method also works
+    	if(Loops.printArray(input).equals(Loops.printReverse(input)))
+		return true;
+			else 
+		return false;*/ 
+    }
+
+
+    public static String printCompress(int[] input){
+        String response = "*** Output ***\n";
+        	response += input[0];
+            for (int i = 1; i < input.length - 1; i++) {
+                if (input[i] != input[i - 1]){
+                	response += "\n" + input[i];	
+                }
+                         }
+            return response;  
+	}
+
+               
+    public static String printPack(char[] input){
+        	String response = "*** Output ***\n";
+        	response += input[0];
+            for (int i = 1; i < input.length; i++) {
+                
+            	if
+                (input[i] == input[i - 1])
+                	response += input[i];
+            	
+            	else if (input[i] != input[i - 1])
+            		response += ", " + input[i];
+
+            }	
+            return response;
+                        
+    } 
 }
+
+
--- src/test/java/io/zipcode/microlabs/loops/LoopsTest.java
+++ src/test/java/io/zipcode/microlabs/loops/LoopsTest.java
@@ -1,6 +1,8 @@
 package io.zipcode.microlabs.loops;
 
 
+import static org.junit.Assert.*;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -24,4 +26,110 @@ public class LoopsTest {
         //:Then
         Assert.assertEquals(expected, actual);
     }
+    
+    
+    @Test
+    public void printLastElementTest(){
+    	// :Given
+    	String [] breakfast = {"Sausage", "Eggs", "Beans",
+                "Bacon", "Tomatoes", "Mushrooms"};
+        String expected = "*** Output ***\n" +
+                "Mushrooms";
+        //:when
+        String actual =Loops.printLastElement(breakfast);
+        //:Then
+        Assert.assertEquals(expected, actual);     		
+    }
+    
+    
+    @Test
+    public void printLastButOneTest(){
+    	// :Given
+    	String [] breakfast = {"Sausage", "Eggs", "Beans",
+                "Bacon", "Tomatoes", "Mushrooms"};
+        String expected = "*** Output ***\n" + 
+        		"Tomatoes";
+        //:when
+        String actual =Loops.printLastButOne(breakfast);
+        //:Then
+        Assert.assertEquals(expected, actual);     		
+    }
+
+
+    @Test
+    public void printReverseTest(){
+    	// :Given
+    	String [] breakfast = {"Sausage", "Eggs", "Beans",
+                "Bacon", "Tomatoes", "Mushrooms"};
+        String expected = "*** Output ***\n" +
+        		"Mushrooms\n" +
+        		"Tomatoes\n" +
+        		"Bacon\n" +
+        		"Beans\n" +
+        		"Eggs\n" +
+        		"Sausage";
+        //:when
+        String actual =Loops.printReverse(breakfast);
+        //:Then
+        Assert.assertEquals(expected, actual);     		
+    }
+
+    
+    @Test
+    public void printPalindromeTest() {
+    	// :Given
+    	{
+    	String [] breakfast = {"Sausage", "Eggs", "Beans",
+                "Bacon", "Tomatoes", "Mushrooms"};
+    	//:when
+    	Loops.printPalindrome(breakfast);
+        //:Then
+        assertFalse(Boolean.FALSE);
+    	}
+    	
+    	{
+        String [] palindromic = {"Sausage", "Eggs", "Beans",
+        		"Beans", "Eggs", "Sausage"};
+        //:when
+        Loops.printPalindrome(palindromic);
+        //:Then
+        assertTrue(Boolean.TRUE);
+        }    	
+    }
+    
+
+    @Test
+    public void printCompressTest() {
+    		// :Given
+			int [] nums = {1,1,3,3,3,2,2,2,1,1,1,1,4,4,4,4};
+			String expected = "*** Output ***\n" + 
+			"1\n" +
+			"3\n" + 
+			"2\n" + 
+			"1\n" + 
+			"4";
+	        String actual =Loops.printCompress(nums);;
+			//:Then
+	        Assert.assertEquals(expected, actual);    
+		}
+    
+
+	@Test
+	public void printPack() {
+    	// :Given		
+    	//String [] letters = {"a", "a", "a", "a", "b", "c", "c", "a", "a", "d","e", "e", "e", "e"};	
+		char [] letters = {'a', 'a', 'a', 'a', 'b', 'c', 'c', 'a', 'a', 'd', 'e', 'e', 'e', 'e'};
+		String expected = "*** Output ***\n" +
+		"aaaa, b, cc, aa, d, eeee";
+		//:when
+	    String actual = Loops.printPack(letters);
+	    System.out.println("Actual: " + actual);
+	    System.out.println("Expected: " + expected);
+		//:Then
+	    Assert.assertEquals(expected, actual); 
+		} 
+
+
 }
+
+
